4aad8c3c28a7bd8c74a854d75499ef6240c3ee2a
Charlotte <charlottefouques10@gmail.com>
2023-12-14 12:24:55 +0100

    delete/put

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,80 @@
-import express from "express"; 
+import express, {Request, Response} from "express"; 
+import {Ad} from './utils/types'; 
 
 const app = express();
 const port = 3000; 
 
-app.get("/", (req, res) => {
+let ads: Ad[] = [
+    {
+      id: 1,
+      title: "Bike to sell",
+      description:
+        "My bike is blue, working fine. I'm selling it because I've got a new one",
+      owner: "bike.seller@gmail.com",
+      price: 100,
+      picture:
+        "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
+      location: "Paris",
+      createdAt: "2023-09-05T10:13:14.755Z",
+    },
+    {
+      id: 2,
+      title: "Car to sell",
+      description:
+        "My car is blue, working fine. I'm selling it because I've got a new one",
+      owner: "car.seller@gmail.com",
+      price: 10000,
+      picture:
+        "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
+      location: "Paris",
+      createdAt: "2023-10-05T10:14:15.922Z",
+    },
+  ];
 
+  app.use(express.json()); 
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!")// envois de la reponse 
 });
 
-app.post("/", (req, res) => {
+app.get("/ads", (req: Request, res: Response) => {
+    res.send(ads)
+  });
 
+app.post("/ads", (req: Request, res: Response) => {
+    const id = ads.length + 1; 
+    const newAd = {... req.body, id, createdAt: new Date().toDateString()}; 
+    ads.push(newAd); 
+    res.send("Request received, check the backend terminal")
+  });
+
+app.delete("/ads/:id", (req : Request, res: Response) => {
+    const idOfAdToDelete = parseInt(req.params.id, 10)
+     if (!ads.find(ad => ad.id === idOfAdToDelete)){
+        res.sendStatus(404)
+     } else {
+       ads = ads.filter((ad) => ad.id !== idOfAdToDelete)
+       res.sendStatus(204)
+     }
+    console.log(idOfAdToDelete)
+})
+
+app.patch("/ads/:id", (req : Request, res: Response) => {
+    const idOfAdToUpdate = parseInt(req.params.id, 10)
+     if (!ads.find(ad => ad.id === idOfAdToUpdate))
+       return res.sendStatus(404)
+
+    const indexOfAdToUpdate = ads.findIndex((ad) => ad.id === idOfAdToUpdate); 
+    ads[indexOfAdToUpdate] = {
+        ...ads[indexOfAdToUpdate], 
+        ... req.body, 
+    }; 
+
+    res.send(ads[indexOfAdToUpdate]); 
+     
+    console.log(idOfAdToUpdate)
+})
+
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`); 
 });
